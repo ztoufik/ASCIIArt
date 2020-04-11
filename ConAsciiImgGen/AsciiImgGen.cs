cc533c1e845cc3e253062e9d2ec558094cd6971f
zoubir toufik <toufikouedtlelat@gmail.com>
2020-04-11 23:36:16 +0200

    Add project files.

---
+++ ConAsciiImgGen/AsciiImgGen.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ConAsciiImgGen
+{
+    static internal class AsciiImgGen
+    {
+         static private string AsciiChars =
+            "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+        static internal int[,][] GetPixelArr( string filepath)
+        {
+            Bitmap image = new Bitmap(filepath);
+            int[,][] outputpixels = new int[image.Width, image.Height][];
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    outputpixels[x, y] = new int[3];
+                    Color color = image.GetPixel(x, y);
+                    outputpixels[x, y][0] = color.R;
+                    outputpixels[x, y][1] = color.G;
+                    outputpixels[x, y][2] = color.B;
+                    
+                    
+                }
+            }
+            return outputpixels;
+        }
+
+        static private char Map(int[] pixel)
+        {
+            int value = pixel.Sum() / 3;
+            value = value * AsciiChars.Length / 256;
+            return AsciiChars[value];
+        }
+
+        static internal string Convert(int[,][] pixels)
+        {
+            StringBuilder output = new StringBuilder();
+
+            for (int y = 0; y < pixels.GetLength(1); y++)
+            {
+                for (int x = 0; x < pixels.GetLength(0); x++)
+                {
+                    output.Append(Map(pixels[x, y]));
+                    output.Append(Map(pixels[x, y]));
+                }
+                output.Append(Environment.NewLine);
+            }
+
+            return output.ToString(); 
+        }
+
+    }
+}
---
+++ ConAsciiImgGen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ConAsciiImgGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" the image file path ? ");
+            string filepath =Console.ReadLine();
+            if (File.Exists(filepath)
+            {
+                int[,][] arrpixels = AsciiImgGen.GetPixelArr(filepath);
+                string AsciiString = AsciiImgGen.Convert(arrpixels);
+
+                File.WriteAllText("./output.txt", AsciiString);
+                System.Diagnostics.Process.Start("CMD.exe", @"/c notepad.exe ./output.txt");
+            }
+
+            else
+            {
+                Console.WriteLine("no such file exists");
+            }
+        }
+
+    }
+}
